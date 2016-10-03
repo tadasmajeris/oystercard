820eaecc06e2ac38415f6664a38b9e94a3a163ae
Frances Maxwell <fmaxwell@gmail.com>
2016-10-03 14:09:42 +0100

    Completed exercise #5 - top-up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,15 @@
 require "oystercard"
+
 describe Oystercard do
+
   it "defaults to balance of 0" do
     expect(subject.balance).to eq 0
   end
+
+  describe "#topup" do
+    it "should allow you to topup" do
+      expect{ subject.topup(10) }.to change{ subject.balance }.by 10
+    end
+  end
+
 end
