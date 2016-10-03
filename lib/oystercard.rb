d40dc4b30c7b7ea2781c191e0aa5de22c60a021d
Frances Maxwell <fmaxwell@gmail.com>
2016-10-03 16:53:56 +0100

    Exercise 11 record start station

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,36 +1,43 @@
 class Oystercard
 
-  attr_reader :balance
+  attr_reader :balance, :start_station
   MAX_BALANCE = 90
   MIN_FARE = 1
 
   def initialize
     @balance = 0
-    @in_journey = false
   end
 
   def topup(amount)
-    fail "Cannot topup! Exceeds maximum balance of #{MAX_BALANCE}." if (balance + amount) > MAX_BALANCE
+    fail_if_exceeds_balance(amount)
     @balance += amount
   end
 
   def in_journey?
-    @in_journey
+    !@start_station.nil?
   end
 
-  def touch_in
+  def touch_in(station)
     fail "Already touched in" if in_journey?
     fail "Insufficient funds" if balance < MIN_FARE
-    @in_journey = true
+    @start_station = station
   end
 
   def touch_out
     fail "Not touched in" unless in_journey?
-    @in_journey = false
     deduct(MIN_FARE)
+    @start_station = nil
   end
 
   private
+
+  def fail_if_exceeds_balance(n)
+    fail "Exceeds maximum balance of #{MAX_BALANCE}." if exceeds_balance(n)
+  end
+
+  def exceeds_balance(n)
+    (balance + n) > MAX_BALANCE
+  end
 
   def deduct(amount)
     @balance -= amount
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,6 +2,8 @@ require "oystercard"
 
 describe Oystercard do
 
+  start_station = :Wimbledon
+
   it "defaults to balance of 0" do
     expect(subject.balance).to eq 0
   end
@@ -9,7 +11,7 @@ describe Oystercard do
   describe "#topup" do
     it "wont allow you to exceed the maximum balance" do
       max_balance = Oystercard::MAX_BALANCE
-      expect{ subject.topup(max_balance+1) }.to raise_error "Cannot topup! Exceeds maximum balance of #{max_balance}."
+      expect{ subject.topup(max_balance+1) }.to raise_error "Exceeds maximum balance of #{max_balance}."
     end
 
     it "should allow you to topup" do
@@ -26,16 +28,21 @@ describe Oystercard do
   describe "#touch_in" do
     it "raises an error if are already in a journey" do
       allow(subject).to receive(:in_journey?).and_return(true)
-      expect{ subject.touch_in }.to raise_error "Already touched in"
+      expect{ subject.touch_in(start_station) }.to raise_error "Already touched in"
     end
     it "raises an error if balance is insufficient" do
       allow(subject).to receive(:balance).and_return(0)
-      expect{ subject.touch_in }.to raise_error "Insufficient funds"
+      expect{ subject.touch_in(start_station) }.to raise_error "Insufficient funds"
     end
     it "should allow you to touch in" do
       subject.topup(Oystercard::MIN_FARE)
-      subject.touch_in
+      subject.touch_in(start_station)
       expect(subject).to be_in_journey
+    end
+    it "should record the name of the station you touched in at" do
+      subject.topup(Oystercard::MIN_FARE)
+      subject.touch_in(start_station)
+      expect(subject.start_station).to eq :Wimbledon
     end
   end
 
@@ -47,7 +54,7 @@ describe Oystercard do
 
     before do
       subject.topup(Oystercard::MIN_FARE)
-      subject.touch_in
+      subject.touch_in(start_station)
     end
 
     it "should allow you to touch out" do
@@ -56,6 +63,10 @@ describe Oystercard do
     end
     it "should deduct balance on touch out" do
       expect{ subject.touch_out }.to change{ subject.balance }.by -Oystercard::MIN_FARE
+    end
+    it "should forget the start station on touch out" do
+      subject.touch_out
+      expect(subject.start_station).to be nil
     end
   end
 end
