aa78ab90244d3dfbe4af3c1fe233a4e472010db9
Glenn <glynester@hotmail.com>
2016-10-04 09:22:43 +0000

    Step 8 - refactored topping up tests into contexts.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,7 +9,7 @@ describe Oystercard do
 
   it { is_expected.to respond_to(:top_up).with(1).argument }
 
-  describe "#top_up" do
+  context "When topping up" do
     it "can top up the balance" do
       expect {subject.top_up 10 }.to change{ subject.balance }.by 10
     end
@@ -18,13 +18,19 @@ describe Oystercard do
       #Oystercard::TOP_UP_LIMIT = 10
       expect(Oystercard::TOP_UP_LIMIT.class).to be Fixnum
     end
-
+  end
+  
+  context "When card is topped up" do
+    before do
+      subject.top_up(Oystercard::TOP_UP_LIMIT)    
+    end
     it "fails if you try to exceed top-up limit" do
-      maximum_balance = Oystercard::TOP_UP_LIMIT
-      subject.top_up(maximum_balance)
-      expect{subject.top_up 1}.to raise_error "Top-up limit of £#{maximum_balance} exceeded."
+      #maximum_balance = Oystercard::TOP_UP_LIMIT
+      #subject.top_up(maximum_balance)
+      expect{subject.top_up 1}.to raise_error "Top-up limit of £#{Oystercard::TOP_UP_LIMIT} exceeded."
     end
   end
+  
 
   describe "#deduct" do
     it "should deduct amount" do
