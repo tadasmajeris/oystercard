8b3f4f69d1f04155596296ce25800e67b1d823c9
Tadas Majeris <tadas.majeris@gmail.com>
2016-10-06 09:46:53 +0100

    Journey class functionality

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,12 +2,24 @@
 class Journey
 
   PENALTY_FARE = 6
+  MIN_FARE = 1
 
   attr_reader :entry_station, :fare
 
-  def initialize(station)
+  def initialize(station=nil)
     @entry_station = station
     @fare = PENALTY_FARE
+    @complete = false
+  end
+
+  def finish(station)
+    @fare = MIN_FARE if entry_station
+    @complete = true
+    self
+  end
+
+  def complete?
+    @complete
   end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,19 +1,44 @@
 require 'journey'
 
 describe Journey do
-  let(:station) { double :station, zone: 2}
-  subject {described_class.new(station)}
+  let(:station) { double :station }
 
   describe 'initialize' do
+    it 'has a penalty fare by default ' do
+      expect(subject.fare).to eq described_class::PENALTY_FARE
+    end
+
+    it 'knows if the journey is not complete' do
+      expect(subject).not_to be_complete
+    end
+  end
+
+  describe '#finish' do
+    it 'returns itself after exiting the journey' do
+      expect(subject.finish(station)).to eq subject
+    end
+  end
+
+  context 'given an entry station' do
+    subject {described_class.new(station)}
 
     it 'has an entry_station' do
       expect(subject.entry_station).to eq station
     end
 
-    it 'has a penalty fare by default ' do
-      expect(subject.fare).to eq described_class::PENALTY_FARE
-    end
+    context 'given an exit station' do
+      let(:exit_station) { double :exit_station }
 
-  end 
+      before { subject.finish exit_station }
+
+      it 'calculates a minimum fare' do
+        expect(subject.fare).to eq described_class::MIN_FARE
+      end
+
+      it 'knows the journey is complete' do
+        expect(subject).to be_complete
+      end
+    end
+  end
 
 end
